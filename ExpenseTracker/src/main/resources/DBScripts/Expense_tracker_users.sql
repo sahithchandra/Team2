b02feea73f91e9cdd707fde45ad79f5e91166540
sahithchandra <sahithchandra008@gmail.com>
2020-03-24 01:09:35 -0400

    Update Expense_tracker_users.sql

--- ExpenseTracker/src/main/resources/DBScripts/Expense_tracker_users.sql
+++ ExpenseTracker/src/main/resources/DBScripts/Expense_tracker_users.sql
@@ -9,6 +9,6 @@ CREATE TABLE `expense_tracker_users` (
   `PASSWORD` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`EMAIL`),
   KEY `USER_ID` (`USER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 
