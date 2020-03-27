f0d2dec8257d747aabcd13da5fb5ee54bd637d1f
sahithchandra <sahithchandra008@gmail.com>
2020-03-27 17:08:20 -0400

    Update Expense_tracker_users.sql

--- ExpenseTracker/src/main/resources/DBScripts/Expense_tracker_users.sql
+++ ExpenseTracker/src/main/resources/DBScripts/Expense_tracker_users.sql
@@ -1,11 +1,9 @@
 DROP TABLE IF EXISTS `expense_tracker_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `expense_tracker_users` (
   `USER_ID` int NOT NULL AUTO_INCREMENT,
-  `USER_NAME` varchar(25) NOT NULL,
-  `EMAIL` varchar(25) NOT NULL,
-  `PASSWORD` varchar(25) DEFAULT NULL,
+  `USER_NAME` varchar(50) NOT NULL,
+  `EMAIL` varchar(80) NOT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`EMAIL`),
   KEY `USER_ID` (`USER_ID`)
 );
